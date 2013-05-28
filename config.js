ba794ec13d448fdf89161b3bad1888ef4172bfd1
Michael Nutt <michael@nuttnet.net>
2013-05-28 14:32:28 -0400

    update settings

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var config = {};
 
-config.LEFT_GARAGE_PIN = 16;
-config.RIGHT_GARAGE_PIN = 11;
+config.LEFT_GARAGE_PIN = 7;
+config.RIGHT_GARAGE_PIN = 16;
 
 config.RELAY_ON = 0;
 config.RELAY_OFF = 1;
-config.RELAY_TIMEOUT = 500;
+config.RELAY_TIMEOUT = 2500;
 
 config.port = 3001;
 
